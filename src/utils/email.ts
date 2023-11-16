a5ee87fdde3f67ea68e519e0171a16fa61ed2d44
Gabriel Rodrigues <ogaaabriel@gmail.com>
2023-11-16 15:41:45 +0000

    Updated transporter creation to work with googleapis lib

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -1,33 +1,66 @@
 import nodemailer from "nodemailer";
+import { google } from "googleapis";
 import { SendEmailCallback } from "../types/email";
 
-const sendEmail = (
+const OAuth2 = google.auth.OAuth2;
+
+const createTransporter = async () => {
+  const oauth2Client = new OAuth2(
+    process.env.OAUTH_CLIENTID,
+    process.env.OAUTH_CLIENT_SECRET,
+    "https://developers.google.com/oauthplayground"
+  );
+
+  oauth2Client.setCredentials({
+    refresh_token: process.env.OAUTH_REFRESH_TOKEN
+  });
+
+  const accessToken = await new Promise((resolve, reject) => {
+    oauth2Client.getAccessToken((err, token) => {
+      if (err) {
+        console.log(err)
+        reject("Failed to create access token :(");
+      }
+      resolve(token);
+    });
+  });
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: process.env.MAIL_USERNAME,
+      accessToken,
+      clientId: process.env.OAUTH_CLIENTID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN
+    }
+  } as nodemailer.TransportOptions);
+
+  return transporter;
+};
+
+const sendEmail = async (
   email: string,
   subject: string,
   content: string,
   callback: SendEmailCallback
 ) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      type: 'OAuth2',
-      user: process.env.MAIL_USERNAME,
-      clientId: process.env.OAUTH_CLIENTID,
-      clientSecret: process.env.OAUTH_CLIENT_SECRET,
-      refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-      accessToken: process.env.OAUTH_ACCESS_TOKEN
-    }
-  } as nodemailer.TransportOptions);
+  try {
+    const transporter = await createTransporter();
 
-  transporter.sendMail(
-    {
-      from: process.env.EMAIL_ADDRESS,
-      to: email,
-      subject,
-      html: content,
-    },
-    callback
-  );
+    transporter.sendMail(
+      {
+        from: process.env.MAIL_USERNAME,
+        to: email,
+        subject,
+        html: content,
+      },
+      callback
+    );
+  } catch (e) {
+    console.log(e)
+  }
 };
 
 export default sendEmail;
